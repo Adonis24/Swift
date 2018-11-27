32f86e73d0f12b6dbd5421508e7bc91d1815478e
r.chernetsov <r.chernetsov@adonis24.ru>
2018-11-27 19:43:18 +0300

    Initial Commit

---
+++ L3 Chernetsov Roman/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L3 Chernetsov Roman
+//
+//  Created by Чернецова Юлия on 27/11/2018.
+//  Copyright © 2018 Чернецов Роман. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
